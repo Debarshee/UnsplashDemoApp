0d8a6eb3bdae5dda21c2ed48c808f70ee01d324e
Debarshee <>
2021-05-21 16:40:47 -0400

    Implemented Firebase in Signup Updated readme

--- UnsplashDemoApp/Module/Sign Up Screen/Controller/SignUpViewController.swift
+++ UnsplashDemoApp/Module/Sign Up Screen/Controller/SignUpViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Debarshee on 5/18/21.
 //
 
+import Firebase
 import UIKit
 
 class SignUpViewController: UIViewController {
@@ -18,17 +19,19 @@ class SignUpViewController: UIViewController {
     @IBOutlet private weak var signUpButton: UIButton!
     @IBOutlet private weak var signUpContainerView: UIView!
     
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstNameTextField.attributedPlaceholder = NSAttributedString(string: "Username",
+        firstNameTextField.attributedPlaceholder = NSAttributedString(string: "First Name",
                                                                       attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        lastNameTextField.attributedPlaceholder = NSAttributedString(string: "Username",
+        lastNameTextField.attributedPlaceholder = NSAttributedString(string: "Last Name",
                                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         usernameTextField.attributedPlaceholder = NSAttributedString(string: "Username",
                                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        emailTextField.attributedPlaceholder = NSAttributedString(string: "Username",
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email",
                                                                   attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Username",
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password",
                                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         signUpContainerView.layer.cornerRadius = 20
         signUpContainerView.clipsToBounds = true
@@ -39,5 +42,42 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction private func signUpButtonClicked(_ sender: UIButton) {
+        if let firstName = firstNameTextField.text, let lastName = lastNameTextField.text, let username = usernameTextField.text {
+            if !firstName.isEmpty && !lastName.isEmpty && !username.isEmpty {
+                Auth.auth().createUser(withEmail: (emailTextField.text ?? ""), password: (passwordTextField.text ?? "")) { [weak self] result, error in
+                    guard let self = self else { return }
+                    if let finalError = error {
+                        print(finalError)
+                        let alert = UIAlertController(title: "Alert", message: "Invalid Email or Password", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    } else {
+                        guard let finalResult = result else { return }
+                        var ref: DocumentReference?
+                        ref = self.db.collection("users").addDocument(data: [
+                            "first name": firstName,
+                            "last name": lastName,
+                            "username": username,
+                            "email": self.emailTextField.text ?? "",
+                            "password": self.passwordTextField.text ?? "",
+                            "uid": finalResult.user.uid
+                        ]) { err in
+                            if let err = err {
+                                print("Error adding document: \(err)")
+                            } else {
+                                guard let reference = ref else { return }
+                                print("Document added with ID: \(reference.documentID)")
+                            }
+                        }
+                    }
+                }
+            } else {
+                let alert = UIAlertController(title: "Alert", message: "User Info Missing", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        } else {
+            print("Error")
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -9,6 +9,7 @@ It is a replica of Unsplash App
 4) Implemented Generic Network layer using URLSession to get the data from the Unsplash API 
 5) Implemented NSCache library to store Images in the local cache 
 6) Implemented a separate storyboard for Login/Signup Screens
+7) Implemented Firebase for the user related functionalities
 
 <p align="center">
 <img src="https://github.com/Debarshee/UnsplashDemoApp/blob/master/UnsplashDemoApp/Resources/Github%20Source/Simulator%20Screen%20Recording%20-%20iPhone%2011%20Pro%20Max%20-%202021-05-14%20at%2015.56.41.gif" width="180" height="320"> 
@@ -18,6 +19,7 @@ It is a replica of Unsplash App
 1) Implemented horizontal collection View to show list of topics
 2) Implemented table view to show photos based on the selected topic
 3) Selecting a photo navigates to PhotoViewScreen
+4) Implemented pagination
 
 ## PhotoViewScreen
 1) Display the selected Photo from the Home Screen
@@ -34,12 +36,14 @@ It is a replica of Unsplash App
 2) Implemented segmented control to show photos of the user, photos liked by user (if any), photo collection of the user (if any)
 3) Implemented multi-cell to display different cell style for collections
 4) Selecting a collection from the cell navigates to CollectionPhotosDisplayScreen
+5) Implemented pagination
 
 ## SearchScreen
 1) Implemented segmented control to display list of photos, collections and users
 2) Implemented SearchBar to search for photos, users or collections based on the segmented control
 3) Implemented multi-cell to display different cell style for collections and users
 4) Selecting a collection from the cell navigates to CollectionPhotosDisplayScreen
+5) Implemented pagination
 
 ## Collection Display Screen
 1) Display all the photos in a collection
@@ -70,6 +74,7 @@ Swift 5
 
 UIKit
 MapKit
+Firebase
 
 ## Supported Devices
 
